7d1f6b1f01bb5feef6c5762a209fdf8cfee7acde
githmith <vuminhthanh2305@gmail.com>
2016-07-15 10:01:12 -0400

    Add code for rotation and visualization

--- test.py
+++ test.py
@@ -2,38 +2,65 @@ import os
 import glob
 import cv2
 import numpy as np
+import math as m
 
 """ Experimenting planar perspective transformation """
-
-def translate ((x,y), dx, dy ):
-	# translation matrix
-	mult = np.array([ [1, 0, dx],
-					  [0, 1, dy],
-					  [0, 0,  1] ])
-	pt1 = np.array([[x],[y],[1]])
-	pt2 = np.dot(mult,pt1)
-
-	return (pt2[0][0],pt2[1][0])
 
 def drawPt(img,pt,color):
 	cv2.circle(img, pt, 3, color, -1)
 
+def ptToMat ((x,y)):
+	return np.array([[x],[y],[1]])
+
+def matToPt (ptMat):
+	return (int(ptMat[0][0]/ptMat[2][0]), int(ptMat[1][0]/ptMat[2][0]))
+
+# translation matrix
+def tl(dx,dy):
+	return np.array([[1, 0, dx],
+					 [0, 1, dy],
+					 [0, 0,  1]])
+
+def rt(angle):
+	p = m.pi
+	a = m.radians(angle)
+	return np.array([[  m.cos(a), m.sin(a),  0 ],
+					 [ -m.sin(a), m.cos(a),  0 ],
+					 [     0    ,    0    ,  1 ]])
+
+def transform(pt, mat):
+	return matToPt(np.dot(mat, ptToMat(pt)))
+
 
 """ ====================== Main code ====================== """
 img = np.zeros((500,1000,3), np.uint8) 
+# cv2.rectangle(img,pt1,pt2,(0,255,0),3)
 blue = (255,0,0)
 green = (0,255,0)
 red = (0,0,255)
+white = (255,255,255)
 
-# cv2.rectangle(img,pt1,pt2,(0,255,0),3)
 
-pt1 = (100,100)
-drawPt(img,pt1,green)
+o = (0,0)
+pts1 = [(150,0),(160,0),(170,0),(170,10),(170,20),(160,20),(150,20),(150,10)]
+dx = 300; dy = 50
+pts2 = [transform(pt, tl(dx,dy)) for pt in pts1]
+pts3 = [transform(pt, rt(60)) for pt in pts1]
 
-dx = 100; dy = 0
-pt2 = translate (pt1,dx,dy)
-print pt2
-drawPt(img,pt2,red)
+# translate the cordinate
+dx = 200
+dy = 250
+o = transform(o, tl(dx,dy))
+pts1 = [transform(pt, tl(dx,dy)) for pt in pts1]
+pts2 = [transform(pt, tl(dx,dy)) for pt in pts2]
+pts3 = [transform(pt, tl(dx,dy)) for pt in pts3]
+
+# Visualize
+cv2.line(img,(o[0]-1000,o[1]),(o[0]+1000,o[1]),white,1)
+cv2.line(img,(o[0],o[1]-1000),(o[0],o[1]+1000),white,1)
+for pt1 in pts1: drawPt(img,pt1,green)
+for pt2 in pts2: drawPt(img,pt2,red)
+for pt3 in pts3: drawPt(img,pt3,red)
 
 cv2.imshow("image",img)
 cv2.waitKey(0)
