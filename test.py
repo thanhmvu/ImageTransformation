79af51086450326abd07d3e6c903752188ee6b2c
githmith <vuminhthanh2305@gmail.com>
2016-07-15 00:22:33 -0400

    Test translation matrix

--- test.py
+++ test.py
@@ -5,22 +5,36 @@ import numpy as np
 
 """ Experimenting planar perspective transformation """
 
-img = np.zeros((1000,1000,3), np.uint8) 
+def translate ((x,y), dx, dy ):
+	# translation matrix
+	mult = np.array([ [1, 0, dx],
+					  [0, 1, dy],
+					  [0, 0,  1] ])
+	pt1 = np.array([[x],[y],[1]])
+	pt2 = np.dot(mult,pt1)
 
-pt1 = (200,300)
-pt2 = (800,700)
+	return (pt2[0][0],pt2[1][0])
 
-cv2.rectangle(img,pt1,pt2,(0,255,0),3)
+def drawPt(img,pt,color):
+	cv2.circle(img, pt, 3, color, -1)
 
-# img = cv2.resize(img, (1000,int(1000*r)))
-# h,w = img.shape[:2]
 
-# M = cv2.getRotationMatrix2D((w/2,h/2), 90, 1) # (center(x,y),angle,scale)
-# img = cv2.warpAffine(img,M,(w,h))
+""" ====================== Main code ====================== """
+img = np.zeros((500,1000,3), np.uint8) 
+blue = (255,0,0)
+green = (0,255,0)
+red = (0,0,255)
+
+# cv2.rectangle(img,pt1,pt2,(0,255,0),3)
+
+pt1 = (100,100)
+drawPt(img,pt1,green)
+
+dx = 100; dy = 0
+pt2 = translate (pt1,dx,dy)
+print pt2
+drawPt(img,pt2,red)
 
 cv2.imshow("image",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# print os.listdir('./*.txt')
-# print glob.glob('./*6.pdf')
