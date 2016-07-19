6d6837c9403ea645fc0871d8c191edd57c60dbf7
githmith <vuminhthanh2305@gmail.com>
2016-07-19 16:05:23 -0400

    Add notes, comments, and further testing

--- test.py
+++ test.py
@@ -23,32 +23,38 @@ def transform(pt, mat):
 
 # translation matrix
 def tl(dx,dy):
-	return np.array([[1, 0, dx],
-					 [0, 1, dy],
-					 [0, 0,  1]])
+	return np.array([[1.0	, 0.0	, float(dx)],
+					 [0.0	, 1.0	, float(dy)],
+					 [0.0	, 0.0	,    1.0   ]])
 
 # rotation matric
 def rt(angle):
 	p = m.pi
 	a = m.radians(angle)
-	return np.array([[  m.cos(a), m.sin(a),  0 ],
-					 [ -m.sin(a), m.cos(a),  0 ],
-					 [     0    ,    0    ,  1 ]])
+	return np.array([[  m.cos(a), m.sin(a),  0.0 ],
+					 [ -m.sin(a), m.cos(a),  0.0 ],
+					 [    0.0   ,   0.0   ,  1.0 ]])
 
 # scale about origin
 def sc(W,H):
-	return np.array([[ W, 0, 0],
-					 [ 0, H, 0],
-					 [ 0, 0, 1]])
+	return np.array([[ float(W)	, 0.0		, 0.0],
+					 [ 0.0		, float(H)	, 0.0],
+					 [ 0.0		, 0.0		, 1.0]])
 
 
 def localCoords(P):
-	# adopt from http://stackoverflow.com/questions/26369618/getting-local-2d-coordinates-of-vertices-of-a-planar-polygon-in-3d-space
-	# len(P) >= 3
+	"""
+	Adopted from http://stackoverflow.com/questions/26369618/getting-local-2d-coordinates-of-vertices-of-a-planar-polygon-in-3d-space
+	
+	Return the local 2D coordinates of each 3D points in set P, given that 
+	- these points are on the same plane in the original 3D coordinate system
+	- the size of P is greater or equal to 3
+	"""
+
 	loc0 = P[0]                      # local origin
-	locx = np.subtract(P[1],loc0)  	 # local X axis
-	normal = np.cross(locx, np.subtract(P[2],loc0)) # vector orthogonal to polygon plane
-	locy = np.cross(normal, locx)      # local Y axis
+	locy = np.subtract(P[len(P)-1],loc0)  	 # local Y axis
+	normal = np.cross(locy, np.subtract(P[1],loc0)) # a vector orthogonal to polygon plane
+	locx = np.cross(normal, locy)      # local X axis
 
 	# normalize
 	locx /= np.linalg.norm(locx)
@@ -57,14 +63,35 @@ def localCoords(P):
 	local_coords = [(np.dot(np.subtract(p,loc0), locx),  # local X coordinate
 	                 np.dot(np.subtract(p,loc0), locy))  # local Y coordinate
 	                for p in P]
+
 	return local_coords
+
+def convertMatrix(M1):
+	""" Convert 4x4 3D perpsective matrix to 3x3 2D perspective matrix """
+
+	# Apply the matrix to transform points
+	srcPts3D = [(0.0,0.0,0.0), (200.0,0.0,0.0), (200.0,100.0,0.0), (0.0,100.0,0.0)]
+	dstPts3D = []
+	for pt in srcPts3D:
+		homoPt = np.array([[pt[0]], [pt[1]], [pt[2]], [1]])
+		out = np.dot(M1,homoPt)
+		dstPt = [out[0][0]/out[3][0],out[1][0]/out[3][0],out[2][0]/out[3][0]]
+		dstPts3D.append(dstPt)
+
+	# Convert 3D coords to 2D coords
+	srcPts2D = np.float32(localCoords(srcPts3D))
+	dstPts2D = np.float32(localCoords(dstPts3D))
+
+	# get 3x3 pespsective matrix
+	M2 = cv2.getPerspectiveTransform(srcPts2D, dstPts2D)
+
+	return M2
 
 
 blue = (255,0,0)
 green = (0,255,0)
 red = (0,0,255)
 white = (255,255,255)
-p = m.pi
 
 """ ====================== Main code ====================== """
 ''' Framework
@@ -76,8 +103,11 @@ Strategy 1:
 => 	original image ----(warpPerspective + 8 pts)---> transformed image
 
 
-Note:
+Notes:
 - cv2.warpPerspective takes a 3x3 float matrix (not 4x4)
+- combine 3x3 matrices, not 4x4 ones
+- applying dot product of all matrices with warpPerspective returns different result from applying them one by one
+- keep in mind that dot product of matrices is not commutative
 
 '''
 
@@ -88,30 +118,33 @@ img[:,:] = (150,150,150)
 offset = 50
 cv2.rectangle(img,(offset,offset),(w-offset,h-offset),blue,3)
 
-# Generate the matrix
+# =============================== perpespective matrix
+# Generate 4x4 perspective matrix
 point = (-w/2, h/2, w/2)
 normal = (-2, 0, 1)
 direction = None
 perspective = (-w, h/2, w)
-M = trans.projection_matrix(point, normal, direction, perspective)
+M1 = trans.projection_matrix(point, normal, direction, perspective)
+# Convert to 3x3 2D matrix
+M1 = convertMatrix(M1)
 
-# Apply the matrix to transform points
-srcPts3D = [(0.0,0.0,0.0), (w*1.0,0.0,0.0), (w*1.0,h*1.0,0.0), (0.0,h*1.0,0.0)]
-dstPts3D = []
-for pt in srcPts3D:
-	homoPt = np.array([[pt[0]], [pt[1]], [pt[2]], [1]])
-	out = np.dot(M,homoPt)
-	dstPt = [out[0][0]/out[3][0],out[1][0]/out[3][0],out[2][0]/out[3][0]]
-	dstPts3D.append(dstPt)
+# =============================== translation
+M2 = tl(100,100)
+
+# =============================== rotation
+M3 = rt(30)
 
 
-# Convert 3D coords to 2D coords
-srcPts2D = np.float32(localCoords(srcPts3D))
-dstPts2D = np.float32(localCoords(dstPts3D))
 
-# Apply the transformation to the image using the src and dst points
-M = cv2.getPerspectiveTransform(srcPts2D, dstPts2D)
-img = cv2.warpPerspective(img,M,(w,h))
+# Apply 3x3 matrix to transform the image
+# =====> one way to do it
+img = cv2.warpPerspective(img,M1,(w,h))
+img = cv2.warpPerspective(img,M2,(w,h))
+img = cv2.warpPerspective(img,M3,(w,h))
+
+# =====> another way to do it. but the result is different
+M = np.dot(M1,M2,M3)
+# img = cv2.warpPerspective(img,M,(w,h))
 
 
 # Visualize 
